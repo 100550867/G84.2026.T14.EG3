3875879ea4177af506fbdf2438275e0bf8f693bc
villanuevadrian <117361825+villanuevadrian@users.noreply.github.com>
2026-04-23 09:45:06 +0200

    Refactor: usar AttributeProjectAcronym en EnterpriseManager

--- src/main/python/uc3m_consulting/attributes/attribute_project_acronym.py
+++ src/main/python/uc3m_consulting/attributes/attribute_project_acronym.py
@@ -2,3 +2,12 @@
 Created By Adrian Villanueva Vergara abr 2026
 Universidad Carlo III de Madrid 
 """
+"""Project acronym attribute."""
+from uc3m_consulting.attributes.attribute import Attribute
+
+
+class AttributeProjectAcronym(Attribute):
+    """Validate project acronym."""
+
+    _validation_pattern = r"^[a-zA-Z0-9]{5,10}$"
+    _error_message = "Invalid acronym"
