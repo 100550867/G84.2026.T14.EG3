5637f47fb75d87333f6dbe2ce3c7023124bc46f0
villanuevadrian <117361825+villanuevadrian@users.noreply.github.com>
2026-04-23 09:49:54 +0200

    Refactir: Eliminado validate_acronym de EnterpriseProject

--- src/main/python/uc3m_consulting/enterprise_project.py
+++ src/main/python/uc3m_consulting/enterprise_project.py
@@ -151,16 +151,6 @@ class EnterpriseProject:
         return company_cif
 
     @staticmethod
-    def validate_project_acronym(project_acronym: str) -> str:
-        """
-        Validate project acronym
-        """
-        acronym_pattern = re.compile(r"^[a-zA-Z0-9]{5,10}$")
-        if not acronym_pattern.fullmatch(project_acronym):
-            raise EnterpriseManagementException("Invalid acronym")
-        return project_acronym
-
-    @staticmethod
     def validate_project_description(project_description: str) -> str:
         """
         Validate project description
